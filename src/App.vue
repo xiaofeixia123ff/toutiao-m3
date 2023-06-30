1dafa7b28cee81c20f15d37dc07d5b00dd61cae6
zyf <2095026158@qq.com>
2023-06-30 15:12:18 +0800

    初始化完成-配置适配REM/把px转为rem/配置axios

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     <van-button type="default">默认按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
     <van-button type="danger">危险按钮</van-button>
+    <div class="box"></div>
     <router-view/>
   </div>
 </template>
@@ -14,4 +15,10 @@ export default {
   name: 'App'
 }
 </script>
-<style lang="less"></style>
+<style scoped lang="less">
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
