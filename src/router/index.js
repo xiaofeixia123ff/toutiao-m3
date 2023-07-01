b4d6c2a787c05d3e76e49973033cc57047638620
zyf <2095026158@qq.com>
2023-07-01 13:48:28 +0800

    TabBar完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,33 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 const routes = [
   {
+    path: '/',
+    // name: 'layout',
+    component: () => import('@/views/layout'),
+    children: [
+      {
+        path: '', // 默认子路由
+        name: 'home',
+        component: () => import('@/views/home')
+      },
+      {
+        path: '/qa',
+        name: 'qa',
+        component: () => import('@/views/qa')
+      },
+      {
+        path: '/video',
+        name: 'video',
+        component: () => import('@/views/video')
+      },
+      {
+        path: '/my',
+        name: 'my',
+        component: () => import('@/views/my')
+      }
+    ]
+  },
+  {
     path: '/login',
     name: 'login',
     component: () => import('@/views/login')
---
+++ src/views/layout/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="layout-container">
+    layout页面
+    <!-- 子路由出口 -->
+    <router-view/>
+    <!-- /子路由出口 -->
+
+    <!-- 标签导航栏 -->
+    <!-- route: 开启路由模式 -->
+    <van-tabbar class="layout-tabbar" v-model="active">
+        <van-tabbar-item to="/">
+            <i slot="icon" class="toutiao toutiao-shouye"></i>
+            <span class="text">首页</span>
+        </van-tabbar-item>
+        <van-tabbar-item to="/qa">
+            <i slot="icon" class="toutiao toutiao-wenda"></i>
+            <span class="text">问答</span>
+        </van-tabbar-item>
+        <van-tabbar-item to="/video">
+            <i slot="icon" class="toutiao toutiao-shipin"></i>
+            <span class="text">视频</span>
+        </van-tabbar-item>
+        <van-tabbar-item to="/my">
+            <i slot="icon" class="toutiao toutiao-wode"></i>
+            <span class="text">我的</span>
+        </van-tabbar-item>
+    </van-tabbar>
+    <!-- /标签导航栏 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutIndex',
+  data () {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.layout-container {
+    .layout-tabbar{
+        i.toutiao {
+            font-size: 40px;
+        }
+    }
+}
+
+</style>
